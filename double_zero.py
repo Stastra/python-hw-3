16ea680a84bfcce5a7057ab1cf85d68b6ccafc26
Stastra <stasto44@gmail.com>
2022-07-23 21:37:13 +0300

    revised

--- double_zero.py
+++ double_zero.py
@@ -22,6 +22,6 @@ def double_zero(array):
     return array_new
 
 
-print(double_zero([1,0,2,3,0,4,5,0])) # [1,0,0,2,3,0,0,4,5,0,0]
-print(double_zero([1,0,0,2,3,0,4,5,0])) # [1,0,0,2,3,0,0,4,5,0,0]
-print(double_zero([1,2,3]))           # [1,2,3]
+print(double_zero([1,0,2,3,0,4,5,0])) 
+print(double_zero([1,0,0,2,3,0,4,5,0])) 
+print(double_zero([1,2,3]))          
